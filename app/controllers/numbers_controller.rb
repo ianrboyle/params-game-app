a4e7da29d75fb250361f0f2abe3edc5038429aa9
Ian Boyle <irboyle21@gmail.com>
2021-10-14 14:11:37 -0400

    changes number guessing name to take in a wilcard user inout

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -1,13 +1,13 @@
 class NumbersController < ApplicationController
   def guess_number 
-    input_value = params["number"]
+    input_value = params["guessed_number"].to_i
     computer_num = 36
 
-    if input_value.to_i == computer_num
+    if input_value == computer_num
       output_message = "You guessed the right number!"
-    elsif input_value.to_i > computer_num
+    elsif input_value > computer_num
       output_message = "Too high!"
-    elsif input_value.to_i< computer_num
+    elsif input_value < computer_num
       output_message = "Too low!"
     end
     render json: {message: output_message}
@@ -16,9 +16,8 @@ class NumbersController < ApplicationController
   def get_sum
     first_input = params["first_number"].to_i
     second_input = params["second_number"].to_i
-
+   
     sum = first_input + second_input
-    render json: {message: sum}
-    
+    render json: {message: "#{first_input} + #{second_input} = #{sum}"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "/get_name" => "names#get_name"
-  get "/numbers_game" => "numbers#guess_number"
+  get "/numbers_game/:guessed_number" => "numbers#guess_number"
   get "sum_numbers/:first_number/:second_number" => "numbers#get_sum"
 end
