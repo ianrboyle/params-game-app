ae3b73994dc93cca1f610e56ed1a232b487c8166
Ian Boyle <irboyle21@gmail.com>
2021-10-14 12:57:32 -0400

    get_name route and action created. renders message if param your_name starts with the letter 'a'

--- app/controllers/names_controller.rb
+++ app/controllers/names_controller.rb
@@ -1,2 +1,12 @@
 class NamesController < ApplicationController
+  def get_name
+    input_value = params["your_name"]
+    input_value = input_value.downcase
+    if input_value.chars.first == "a"
+      render json: {message: "Your name starts with 'a'!"}
+    else
+      render json: {message: "Your name does not start with 'a'!"}
+    end
+  end
+
 end
